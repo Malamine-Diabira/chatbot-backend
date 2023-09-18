00b3fd00282e4e0eec57996a24f11f82cc0145f4
Malamine-Diabira <malamine.diabira11@gmail.com>
2023-09-18 12:58:32 +0000

    Premier commit

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+AIRTABLE_BASE_ID = os.environ.get('https://airtable.com/appGlSqGhgP18ZMnJ/tblLEBUlGftyl87lM/viwgVQesfYuBefNeb?blocks=hide')
+AIRTABLE_TABLE_NAME = os.environ.get('Conversations')
+AIRTABLE_TOKEN = os.environ.get('patX6FCk0rIa7Kecw.10830f2c9122e199150a254abb68f18c307f8a6c6b989d35fd354cc106568145')
+
+OPENAI_API_KEY = os.environ.get('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')
